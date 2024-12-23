de5f734107a5b9beae9c5ccb14a4e7fb2194a255
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2024-12-23 16:06:10 +0000

    Utils

--- internal/utils/utlis.go
+++ internal/utils/utlis.go
@@ -35,35 +35,56 @@ func LoggerInit() (*os.File, error) {
 	return logFile, nil
 }
 
-func GetHTML(url string) (string, error) {
+type HTTPResponse struct {
+	StatusCode      int
+	URL             string
+	Redirected      bool
+	HTML            string
+	RedirectHistory []string
+}
+
+func GetHTML(url string) (HTTPResponse, error) {
+	var HttpResponse HTTPResponse
+	HttpResponse.URL = url
+
 	// Create a request object
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		e := fmt.Errorf("[debug] error creating request: %v", err)
-		return "", e
+		return HttpResponse, e
 	}
-
 	// Create a new HTTP Client
-	client := &http.Client{}
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			HttpResponse.Redirected = true
+			// Append previous URLs to the redirect history
+			for _, r := range via {
+				HttpResponse.RedirectHistory = append(HttpResponse.RedirectHistory, r.URL.String())
+			}
+			HttpResponse.RedirectHistory = append(HttpResponse.RedirectHistory, req.URL.String())
+			return nil
+		},
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		e := fmt.Errorf("[debug] error making request: %v", err)
-		return "", e
+		return HttpResponse, e
 	}
 	defer resp.Body.Close()
+	// Save status code
+	HttpResponse.StatusCode = resp.StatusCode
 
 	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		e := fmt.Errorf("[debug] error reading response body: %v", err)
-		return "", e
+		return HttpResponse, e
 	}
-
 	// Response is OK
 	if resp.StatusCode != http.StatusOK {
 		e := fmt.Errorf("[debug] HTTP code error %d", resp.StatusCode)
-		return "", e
+		return HttpResponse, e
 	}
-	html := string(body)
-	return html, nil
+	HttpResponse.HTML = string(body)
+	return HttpResponse, nil
 }
